42eb545775ace08c0860dfafa5a28d1d0f349787
Macbook Air <macbookair@macbook-niko.local>
2025-09-03 19:44:08 -0500

    Primer commit: menú principal implementado

---
+++ xor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Bienvenidx al encriptador XOR, ingrese la opción que desea realizar\n" );
+    printf("1. Encriptar\n2. Desencriptar\n" );
+    return 0;
+}
+
+int encrypt(char) {
+
+}
+
+int decrypt(char) {
+
+}
